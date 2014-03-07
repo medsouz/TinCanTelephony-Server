c3cc37830d3296e0020f388df02d7bb80fdfac76
medsouz <medsouz99@gmail.com>
2014-03-06 22:12:26 -0500

    Added friends/friend requests
    TODO: Write a packet so that client can activate this code

--- friends.js
+++ friends.js
@@ -1,7 +1,8 @@
 //Setup SQLite Table
-db.run("create table if not exists friends(id INTEGER PRIMARY KEY, senderID INTEGER, receiverID INTEGER, message TEXT, accepted BOOLEAN, timestamp DATETIME)");
+db.run("create table if not exists friends(senderID INTEGER, receiverID INTEGER, message TEXT, accepted BOOLEAN, timestamp DATETIME)");
 
-var users = require("./users.js");
+var users = require("./users.js"),
+	moment = require("moment");
 
 var Friends = {};
 
@@ -10,9 +11,11 @@ Friends.getFriendConnection = function (userA, userB, callback) {
 		if(idA != -1) {
 			users.getID(userB, function (idB) {
 				if(idB != -1) {
-					db.get("SELECT * FROM friends WHERE (senderID = ?1 AND receiverID = ?2) OR (senderID = ?2 AND receiverID = ?1)", idA, idB, function(err, row) {
+					db.get("SELECT *, (SELECT COUNT(*) FROM friends WHERE (senderID = ?1 AND receiverID = ?2) OR (senderID = ?2 AND receiverID = ?1)) AS id FROM friends", idA, idB, function(err, row) {
 						if(!err) {
 							callback(row);
+						} else {
+							console.log(err);
 						}
 					});
 				}
@@ -21,8 +24,26 @@ Friends.getFriendConnection = function (userA, userB, callback) {
 	});
 }
 
-Friends.addFriend = function(user, friend) {
-	
+Friends.addFriend = function(user, friend, /* optional */ message) {
+	users.getID(user, function (idA) {
+		if(idA != -1) {
+			users.getID(friend, function (idB) {
+				if(idB != -1) {
+					Friends.getFriendConnection(user, friend, function (row) {//check to see if a friend request has already been sent to make this connection
+						if(row) {//if a connection already exists...
+							if(idA == row.receiverID && idB == row.senderID){//The friend is accepting! If this is false then someone tried to resend a friend request
+								db.run("UPDATE friends SET accepted = ?3 WHERE (senderID = ?1 AND receiverID = ?2) OR (senderID = ?2 AND receiverID = ?1)", idA, idB, 1);
+							} else {
+								console.log(user + " tried to accept a friend request he sent...");
+							}
+						} else {//no connection
+							db.run("INSERT INTO friends VALUES (?, ?, ?, 0, ?)", idA, idB, message, moment().format("YY-MM-DD HH:mm:SS"));
+						}
+					});
+				}
+			});
+		}
+	});
 }
 
 module.exports = Friends;
