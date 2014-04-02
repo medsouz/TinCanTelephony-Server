020305addec9ba901a03b59b58fc8fda8db06913
medsouz <medsouz99@gmail.com>
2014-04-01 21:53:21 -0400

    Fixed typo

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	host: "0.0.0.0",
 	port: 9999,
-	noAuth: true,//If false the server will not verify the user's login with Mojang's server. WARNING: This allows players to impersonate others!
+	noAuth: true,//If true the server will not verify the user's login with Mojang's server. WARNING: This allows players to impersonate others!
 	verbose: true
 }
